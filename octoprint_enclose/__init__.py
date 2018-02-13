6462c1f6e2b198960868e650301b9a002762d0e4
Darko Grozdanovski <darkogrozdanovski@gmail.com>
2018-02-14 00:22:33 +0100

    initial version

--- octoprint_enclose/__init__.py
+++ octoprint_enclose/__init__.py
@@ -1,37 +1,71 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
-
 import octoprint.plugin
 
-class EnclosePlugin(octoprint.plugin.SettingsPlugin,
-                    octoprint.plugin.AssetPlugin,
-                    octoprint.plugin.TemplatePlugin):
+import requests
+
+class EnclosePlugin(octoprint.plugin.StartupPlugin,
+					octoprint.plugin.ProgressPlugin,
+					octoprint.plugin.EventHandlerPlugin,
+					octoprint.plugin.SettingsPlugin,
+					octoprint.plugin.TemplatePlugin):
+	def on_after_startup(self):
+		self._logger.info("OctoEnclosure (on host: %s)" % self._settings.get(["hostname"]))
 
 	##~~ SettingsPlugin mixin
 
 	def get_settings_defaults(self):
 		return dict(
-			# put your plugin's default settings here
+			hostname=""
 		)
 
-	##~~ AssetPlugin mixin
+	def get_template_configs(self):
+		return [
+			dict(type="settings", custom_bindings=False)
+		]
 
-	def get_assets(self):
-		# Define your plugin's asset files to automatically include in the
-		# core UI here.
+	def get_template_vars(self):
 		return dict(
-			js=["js/enclose.js"],
-			css=["css/enclose.css"],
-			less=["less/enclose.less"]
 		)
+
+	def on_print_progress(self, storage, path, progress):
+		self._logger.info("OctoEnclosure print progress: %s)" % progress)
+		self.execute_request("keepAlive")
+
+	def on_event(self, event, payload):
+		if event == octoprint.events.Events.PRINT_STARTED:
+			self._logger.info("print started  %s)" % event)
+			self.execute_request("ledOn?r=1023&g=1023&b=1023")
+			self.execute_request("fanOn")
+		elif event == octoprint.events.Events.PRINT_DONE:
+			self._logger.info("print done %s)" % event)
+			self.execute_request("ledOn?r=200&g=1023&b=200")
+		elif event in [octoprint.events.Events.CONNECTED,
+					   octoprint.events.Events.DISCONNECTED,
+					   octoprint.events.Events.PRINT_CANCELLED,
+					   octoprint.events.Events.PRINT_PAUSED,
+					   octoprint.events.Events.PRINT_RESUMED]:
+			self._logger.info("event action needed %s" % event)
+			self.execute_request("ledOn?r=800&g=800&b=800")
+		elif event in [octoprint.events.Events.PRINT_FAILED,
+					   octoprint.events.Events.ERROR]:
+			self._logger.info("event error %s" % event)
+			self.execute_request("ledOn?r=1023&g=200&b=200")
+		else:
+			self._logger.info("event received %s" % event)
+
+	def execute_request(self, path):
+		url = ""
+		try:
+			hostname = self._settings.get(["hostname"])
+			url = "%s/%s" % (hostname, path)
+			if hostname:
+				self._logger.info("fetching data: %s)" % url)
+				r = requests.get(url)
+				self._logger.info("Response status: %s)" % r.status_code)
+		except:
+			self._logger.info("Error executing request: %s)" % url)
 
 	##~~ Softwareupdate hook
 
@@ -56,11 +90,6 @@ class EnclosePlugin(octoprint.plugin.SettingsPlugin,
 		)
 
 
-# If you want your plugin to be registered within OctoPrint under a different name than what you defined in setup.py
-# ("OctoPrint-PluginSkeleton"), you may define that here. Same goes for the other metadata derived from setup.py that
-# can be overwritten via __plugin_xyz__ control properties. See the documentation for that.
-__plugin_name__ = "Enclose Plugin"
-
 def __plugin_load__():
 	global __plugin_implementation__
 	__plugin_implementation__ = EnclosePlugin()
@@ -69,4 +98,3 @@ def __plugin_load__():
 	__plugin_hooks__ = {
 		"octoprint.plugin.softwareupdate.check_config": __plugin_implementation__.get_update_information
 	}
-
